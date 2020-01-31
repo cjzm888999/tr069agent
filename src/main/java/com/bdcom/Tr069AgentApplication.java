bfa3747be5d281679a6a2f0e4ceafaefef15bcb8
cjzm888999 <404436080@qq.com>
2020-01-31 21:37:25 +0800

    Initial commit

---
+++ src/main/java/com/bdcom/Tr069AgentApplication.java
@@ -0,0 +1,13 @@
+package com.bdcom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tr069AgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tr069AgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bdcom/Tr069agentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdcom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tr069agentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
